139550abfe4d02b6cb0deca36bd1f919c66291bb
shreyasbhat95 <shreyasbhat95@gmail.com>
2020-12-22 19:10:45 +0530

    Update a.vb

--- a.vb
+++ a.vb
@@ -1,5 +1,23 @@
-if (a=b) and (c=d) and (e=f) then
-Console.Writeline("a is equal to b")
-Console.Writeline("c is equal to d")
-Console.Writeline("e is equal to f")
-end if
+'if (a=b) and (c=d) and (e=f) then
+'Console.Writeline("a is equal to b")
+'Console.Writeline("c is equal to d")
+'Console.Writeline("e is equal to f")
+'end if
+
+Private Sub Test()
+  if (a =b) then
+    Console.Writeline("a is equal to b")
+  end if
+EndSub
+ 
+Private Sub Test2()
+  if (c = d) then
+    Console.Writeline("c is equal to d")
+  end if
+EndSub
+    
+Private Sub Test3()
+  if (e = f) then
+    Console.Writeline("e is equal to f")
+  end if
+EndSub
