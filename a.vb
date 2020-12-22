473d14e65dc2c298851da7dacc18de4fa9d11fb9
shreyasbhat95 <shreyasbhat95@gmail.com>
2020-12-22 19:08:15 +0530

    Update a.vb

--- a.vb
+++ a.vb
@@ -1,15 +1,5 @@
-'if (a=b) and (c=d) and (e=f) then
-'Console.Writeline("a is equal to b")
-'Console.Writeline("c is equal to d")
-'Console.Writeline("e is equal to f")
-'end if
-if (a =b) then
+if (a=b) and (c=d) and (e=f) then
 Console.Writeline("a is equal to b")
-end if
-if (c = d) then
 Console.Writeline("c is equal to d")
-end if
-if (e = f) then
 Console.Writeline("e is equal to f")
-Console.Writeline("a c and e are not equal")
 end if
